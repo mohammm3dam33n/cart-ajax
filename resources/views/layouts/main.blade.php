e86a8a73bdd4e9045ecede4c5eaeb57740bfd1ba
mohammm3dam33n <mohammm3dam33n@gmail.com>
2022-02-05 22:28:45 +0200

    first commit

---
+++ resources/views/layouts/main.blade.php
@@ -0,0 +1,157 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Laravel Add To Cart Function - ItSolutionStuff.com</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js"></script>
+
+    <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="row">
+            <div class="col-lg-12 col-sm-12 col-12 main-section">
+                <div class="dropdown">
+                    <button type="button" class="btn btn-info" data-toggle="dropdown">
+                        <i class="fa fa-shopping-cart" aria-hidden="true"></i> Cart <span
+                            class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span>
+                    </button>
+                    <div class="dropdown-menu">
+                        <div class="row total-header-section">
+                            <div class="col-lg-6 col-sm-6 col-6">
+                                <i class="fa fa-shopping-cart" aria-hidden="true"></i> <span
+                                    class="badge badge-pill badge-danger">{{ count((array) session('cart')) }}</span>
+                            </div>
+                            @php $total = 0 @endphp
+                            @foreach ((array) session('cart') as $id => $details)
+                                @php $total += $details['price'] * $details['quantity'] @endphp
+                            @endforeach
+                            <div class="col-lg-6 col-sm-6 col-6 total-section text-right">
+                                <p>Total: <span class="text-info">$ {{ $total }}</span></p>
+                            </div>
+                        </div>
+                        @if (session('cart'))
+                            @foreach (session('cart') as $id => $details)
+                                <div class="row cart-detail">
+                                    <div class="col-lg-4 col-sm-4 col-4 cart-detail-img">
+                                        <img src="{{ $details['image'] }}" />
+                                    </div>
+                                    <div class="col-lg-8 col-sm-8 col-8 cart-detail-product">
+                                        <p>{{ $details['name'] }}</p>
+                                        <span class="price text-info"> ${{ $details['price'] }}</span> <span
+                                            class="count"> Quantity:{{ $details['quantity'] }}</span>
+                                    </div>
+                                </div>
+                            @endforeach
+                        @endif
+                        <div class="row">
+                            <div class="col-lg-12 col-sm-12 col-12 text-center checkout">
+                                <a href="{{ route('cart') }}" class="btn btn-primary btn-block">View all</a>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <br />
+    <div class="container">
+
+        @if (session('success'))
+            <div class="alert alert-success">
+                {{ session('success') }}
+            </div>
+        @endif
+
+        @yield('content')
+    </div>
+
+
+
+    <script type="text/javascript">
+        /*
+        |--------------------------------------------------------------------------
+        | Start Cart
+        |--------------------------------------------------------------------------
+        |
+        */
+
+/*
+        //Start Js Add Cart
+        $(".add-to-cart").click(function(e) {
+            console.log('adddddddd');
+            //Start ajax
+
+            //end ajax
+        });
+        //Start Js Add Cart
+*/
+
+
+        //Start Js Update Cart
+        $(".update-cart").change(function(e) {
+            e.preventDefault();
+            var ele = $(this);
+
+            //Start ajax
+            $.ajax({
+                url: '{{ route('update.cart') }}',
+                method: "patch",
+                data: {
+                    _token: '{{ csrf_token() }}',
+                    id: ele.parents("tr").attr("data-id"),
+                    quantity: ele.parents("tr").find(".quantity").val()
+                },
+                success: function(response) {
+                    window.location.reload();
+                }
+            }); //end ajax
+        });
+        //Start Js Update Cart
+
+        //Start Js remove Cart
+        $(".remove-from-cart").click(function(e) {
+            e.preventDefault();
+
+            var ele = $(this);
+
+            if (confirm("Are you sure want to remove?")) {
+
+                //Start ajax
+                $.ajax({
+                    url: '{{ route('remove.from.cart') }}',
+                    method: "DELETE",
+                    data: {
+                        _token: '{{ csrf_token() }}',
+                        id: ele.parents("tr").attr("data-id")
+                    },
+                    success: function(response) {
+                        window.location.reload();
+                    }
+                }); //end ajax
+            }
+        });
+        //Start Js remove Cart
+
+
+
+        /*
+        |--------------------------------------------------------------------------
+        | End Cart
+        |--------------------------------------------------------------------------
+        |
+        */
+    </script>
+
+
+
+</body>
+
+</html>
---
+++ resources/views/products.blade.php
@@ -0,0 +1,24 @@
+@extends('layouts.main')
+
+@section('content')
+    <div class="row">
+        @foreach ($products as $product)
+            <div class="col-xs-18 col-sm-6 col-md-3">
+                <div class="thumbnail">
+                    <img src="{{ $product->image }}" alt="">
+                    <div class="caption">
+                        <h4>{{ $product->name }}</h4>
+                        <p>{{ $product->description }}</p>
+                        <p><strong>Price: </strong> {{ $product->price }}$</p>
+                        <p><strong>id: </strong> {{ $product->id }}$</p>
+                        <p class="btn-holder"><a href="{{ route('add.to.cart', $product->id) }}" class="btn btn-warning btn-block text-center" role="button">Add to cart</a> </p>
+
+                        {{-- <button  class="btn btn-warning btn-block text-center add-to-cart">Add to cart</button> --}}
+
+                    </div>
+                </div>
+            </div>
+        @endforeach
+    </div>
+
+@endsection
